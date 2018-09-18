a0a6213f0c97a20e8202608f431e9ebf492ac61d
Link Dupont <link@sub-pop.net>
2018-09-18 13:00:59 -0700

    Add default ordering on ReleaseTypeViewSet
    Silences a warning about results during pagination

--- fpdc/releases/views.py
+++ fpdc/releases/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework import filters
 
 from fpdc.releases.models import ReleaseType
 from fpdc.releases.serializers import ReleaseTypeSerializer
@@ -7,3 +8,6 @@ from fpdc.releases.serializers import ReleaseTypeSerializer
 class ReleaseTypeViewSet(viewsets.ModelViewSet):
     queryset = ReleaseType.objects.all()
     serializer_class = ReleaseTypeSerializer
+    filter_backends = (filters.OrderingFilter,)
+    ordering_fields = "__all__"
+    ordering = ("id",)
