10fc85536e24693ff7c33d2d7d613643ee937611
Link Dupont <link@sub-pop.net>
2018-09-25 21:08:43 -0700

    Add DEFAULT_PERMISSION_CLASSES to settings
    The default is set to `IsAuthenticatedOrReadOnly`. From the docs:
    
    The IsAuthenticatedOrReadOnly will allow authenticated users to perform any request. Requests for unauthorised users will only be permitted if the request method is one of the "safe" methods; GET, HEAD or OPTIONS.

--- fpdc/releases/tests/test_views.py
+++ fpdc/releases/tests/test_views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.models import User
+
 from mixer.backend.django import mixer
 from rest_framework import status
 from rest_framework.reverse import reverse
@@ -17,8 +19,13 @@ DATA = {
 
 
 class ReleaseViewTests(APITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = mixer.blend(User)
+
     def test_create_release(self):
         url = reverse("v1:release-list")
+        self.client.force_authenticate(self.__class__.user)
         response = self.client.post(url, DATA, format="json")
         assert response.status_code == status.HTTP_201_CREATED
         assert Release.objects.count() == 1
@@ -27,6 +34,7 @@ class ReleaseViewTests(APITestCase):
     def test_update_release(self):
         release = mixer.blend(Release)
         url = reverse("v1:release-detail", kwargs={"pk": release.pk})
+        self.client.force_authenticate(self.__class__.user)
         response = self.client.put(url, DATA, format="json")
         assert response.status_code == status.HTTP_200_OK
         assert Release.objects.count() == 1
@@ -36,6 +44,7 @@ class ReleaseViewTests(APITestCase):
         release = mixer.blend(Release)
         url = reverse("v1:release-detail", kwargs={"pk": release.pk})
         data = {"release_id": "fedora-28"}
+        self.client.force_authenticate(self.__class__.user)
         response = self.client.patch(url, data, format="json")
         assert response.status_code == status.HTTP_200_OK
         assert Release.objects.count() == 1
@@ -45,6 +54,7 @@ class ReleaseViewTests(APITestCase):
         release = mixer.blend(Release)
         url = reverse("v1:release-detail", kwargs={"pk": release.pk})
         assert Release.objects.count() == 1
+        self.client.force_authenticate(self.__class__.user)
         response = self.client.delete(url, format="json")
         assert response.status_code == status.HTTP_204_NO_CONTENT
         assert Release.objects.count() == 0
@@ -62,3 +72,28 @@ class ReleaseViewTests(APITestCase):
         response = self.client.get(url, format="json")
         assert response.status_code == status.HTTP_200_OK
         assert Release.objects.count() == 5
+
+    def test_create_release_unauthenticated(self):
+        url = reverse("v1:release-list")
+        response = self.client.post(url, DATA, format="json")
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    def test_update_release_unauthenticated(self):
+        release = mixer.blend(Release)
+        url = reverse("v1:release-detail", kwargs={"pk": release.pk})
+        response = self.client.put(url, DATA, format="json")
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    def test_partial_update_release_unauthenticated(self):
+        release = mixer.blend(Release)
+        url = reverse("v1:release-detail", kwargs={"pk": release.pk})
+        data = {"release_id": "fedora-28"}
+        response = self.client.patch(url, data, format="json")
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    def test_delete_release_unauthenticated(self):
+        release = mixer.blend(Release)
+        url = reverse("v1:release-detail", kwargs={"pk": release.pk})
+        assert Release.objects.count() == 1
+        response = self.client.delete(url, format="json")
+        assert response.status_code == status.HTTP_403_FORBIDDEN
--- fpdc/settings/base.py
+++ fpdc/settings/base.py
@@ -78,6 +78,7 @@ REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
     "DEFAULT_VERSIONING_CLASS": "rest_framework.versioning.NamespaceVersioning",
+    "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticatedOrReadOnly",),
 }
 
 
