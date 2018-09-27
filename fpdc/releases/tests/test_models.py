07acc27fb08a46dfb40a9d96663e57adbc2e50ef
Link Dupont <link@sub-pop.net>
2018-09-26 22:51:24 -0700

    Add tests for 'development' and 'ga' status
    Brings in the 'freezegun' module to force the date.today() to travel
    through time.

--- fpdc/releases/tests/test_models.py
+++ fpdc/releases/tests/test_models.py
@@ -1,6 +1,8 @@
-import pytest
 from datetime import date
+
+import pytest
 from django.test import TestCase
+from freezegun import freeze_time
 from mixer.backend.django import mixer
 
 from fpdc.releases.models import Release
@@ -20,9 +22,26 @@ class ReleaseTests(TestCase):
         r = Release.objects.get(short__exact="fedora")
         assert r.short == "fedora"
 
-    def test_calculated_fields(self):
+    @freeze_time("2018-06-01")
+    def test_calculated_fields_is_eol(self):
         """Ensure we can query calculated fields."""
         mixer.blend(Release, eol_date=date(2018, 5, 29), release_date=date(2017, 7, 11))
         r = Release.objects.get(status__exact="eol")
         assert r.status == "eol"
+        assert r.active is False
+
+    @freeze_time("2018-05-01")
+    def test_calculated_fields_is_ga(self):
+        """Ensure we can query calculated fields."""
+        mixer.blend(Release, eol_date=date(2018, 5, 29), release_date=date(2017, 7, 11))
+        r = Release.objects.get(status__exact="ga")
+        assert r.status == "ga"
+        assert r.active is True
+
+    @freeze_time("2017-06-01")
+    def test_calculated_fields_is_development(self):
+        """Ensure we can query calculated fields."""
+        mixer.blend(Release, eol_date=date(2018, 5, 29), release_date=date(2017, 7, 11))
+        r = Release.objects.get(status__exact="development")
+        assert r.status == "development"
         assert r.active is False
